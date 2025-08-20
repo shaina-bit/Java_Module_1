009c2fbc6289804df92059a586cb663b12d59ca7
Shaina-bit <shainanoushad0305@gmail.com>
2025-08-20 19:33:07 +0530

    adding two new h/w files

--- Greater.java
+++ Greater.java
@@ -1,14 +1,15 @@
 package Java_Module_1;
 import java.util.*;
-class Greater{
+public class greater {
     public static void main(String[] args) {
         Scanner in= new Scanner(System.in);
-        System.out.println("Enter a number");
+        System.out.println("Enter two numbers: ");
         int a= in.nextInt();
-        if(a>0){
-            System.out.println(a + "is positive");
-        } else{
-            System.out.println(a + "is negative");
+        int b= in.nextInt();
+        if(a>b){
+            System.out.println(a + "is greater than" + b);
+        }else{
+            System.out.println(b + "is greater than" + a);
         }
         in.close();
     }
---
+++ array2.java
@@ -0,0 +1,18 @@
+package Java_Module_1;
+
+public class array2 {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        arr[0]= 1;//square of 1
+        arr[1]= 4;//square of 2
+        arr[2]= 9;//square of 3 and so on
+        arr[3]= 16;
+        arr[4]= 25;
+        arr[5]= 36;
+        arr[6]= 49;
+        arr[7]= 64;
+        arr[8]= 81;
+        arr[9]= 100;
+        System.out.println(arr[9]);
+    }
+}
