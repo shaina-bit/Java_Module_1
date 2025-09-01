8e3dfffed8524eed98a6e0031fde43b06fa14fbc
Shaina-bit <shainanoushad0305@gmail.com>
2025-09-01 12:11:23 +0530

    added first string program

---
+++ str1.java
@@ -0,0 +1,10 @@
+package Java_Module_1;
+
+public class str1 {
+    public static void main(String[] args) {
+        String a= "Hello";
+        System.out.println(a.toUpperCase());
+        System.out.println(a.toLowerCase());
+        
+    }
+}
