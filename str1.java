ec7032c25b13ce5ff0dd51c90c3147b62893131f
Shaina-bit <shainanoushad0305@gmail.com>
2025-09-01 12:18:31 +0530

    added new features

--- str1.java
+++ str1.java
@@ -3,8 +3,16 @@ package Java_Module_1;
 public class str1 {
     public static void main(String[] args) {
         String a= "Hello";
+        String b= "hello";
         System.out.println(a.toUpperCase());
         System.out.println(a.toLowerCase());
-        
+        System.out.println(a.length());
+        System.out.println(a.charAt(0));
+        if (a.equalsIgnoreCase(b)){
+            System.out.println("Both the strings are equal.");
+        }
+        else{
+            System.out.println("They are not equal.");
+        }
     }
 }
