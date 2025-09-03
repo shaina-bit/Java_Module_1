e43360a3f7574ac8f609dc9ce640c25c5707fd3e
Shaina-bit <shainanoushad0305@gmail.com>
2025-09-03 14:54:12 +0530

    added replace o with a program

---
+++ str4.java
@@ -0,0 +1,13 @@
+package Java_Module_1;
+public class str4 {
+    public static void main(String[] args) {
+        // Define an original string
+        String orgString = "Hello World";
+
+        // Replace 'o' with 'p' in the original string
+        String replacedString = orgString.replace("o", "p");
+
+        // Print the replaced string
+        System.out.println(replacedString);
+    }
+}
